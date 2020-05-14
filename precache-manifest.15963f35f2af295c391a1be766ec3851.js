8f34cf545d03bd44951ef1048ecd81c1b376cbae
marton <mpochavillo@gmail.com>
2020-05-14 22:50:42 +0800

    Updates

---
+++ precache-manifest.15963f35f2af295c391a1be766ec3851.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84a1cd82c832a28b44a82d0b51d954d3",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "32d4f3f8b14dd78d2c14",
+    "url": "/monster-rolodex/static/css/main.2d647ce0.chunk.css"
+  },
+  {
+    "revision": "1b163c6dfb305b2c587f",
+    "url": "/monster-rolodex/static/js/2.b0e7918a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.b0e7918a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32d4f3f8b14dd78d2c14",
+    "url": "/monster-rolodex/static/js/main.7bc83ee0.chunk.js"
+  },
+  {
+    "revision": "1db901b49b16624dee18",
+    "url": "/monster-rolodex/static/js/runtime-main.b410029c.js"
+  }
+]);
