8ba6738aeee8822fca203b0ee2d72669bdfa4929
marton <mpochavillo@gmail.com>
2020-05-14 22:51:48 +0800

    deploy the app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
 import React from "react";
 import "./App.css";
 import CardList from "./components/card-list/card-list.component";
+import SearchBox from "./components/search-box/search-box.component";
 
 class App extends React.Component {
   state = {
     monsters: [],
+    searchField: ""
   };
 
   componentDidMount() {
     fetch("https://jsonplaceholder.typicode.com/users")
-      .then((response) => response.json())
-      .then((users) => this.setState({ monsters: users }));
+      .then(response => response.json())
+      .then(users => this.setState({ monsters: users }));
   }
+
+  handleChange = e => {
+    this.setState({ searchField: e.target.value });
+  };
+
   render() {
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter(monster => monster.name.toLowerCase().includes(searchField.toLowerCase()));
     return (
       <div className="App">
-        <CardList>
-          {this.state.monsters.map((monster, index) => {
-            return <h2 key={index}>{monster.name}</h2>;
-          })}
-        </CardList>
+        <h1>Monsters Rolodex</h1>
+        <SearchBox placeholder="Search Monsters" handleChange={this.handleChange} />
+        <CardList monsters={filteredMonsters}></CardList>
       </div>
     );
   }
--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,8 +1,15 @@
 import React from "react";
 import "./card-list.styles.css";
+import Card from "../card/card.component";
 
 function CardList(props) {
-  return <div className="card-list">{props.children}</div>;
+  return (
+    <div className="card-list">
+      {props.monsters.map(monster => {
+        return <Card key={monster.id} monster={monster} />;
+      })}
+    </div>
+  );
 }
 
 export default CardList;
